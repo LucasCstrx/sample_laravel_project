bcaead7296bfd68931ca4ee39020e9589fa32bd2
ccastro <ccastro@uic.edu.ph>
2025-12-30 20:37:15 +0800

    Initial Laravel project commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Ninja Network</title>
+</head>
+<body>
+    <h1>Welcome to Ninja Network</h1>
+    <p>Click the button below to view</p>
+
+    <a href="" class="btn">Find Ninjas!</a>
+</body>
+</html>
