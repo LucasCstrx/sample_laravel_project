a4900517efef77b7380747171f6c4e636f77fa64
ccastro <ccastro@uic.edu.ph>
2026-01-14 17:38:16 +0800

    until lesson 7

--- resources/views/ninjas/index.blade.php
+++ resources/views/ninjas/index.blade.php
@@ -1,23 +1,18 @@
-<html lang="en">
-<head>
-   <meta charset="UTF-8">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Ninja Network | Home</title>
-</head>
-<body>
-   <h2>Currently Available Ninjas</h2>
-   
-   @if ($greeting == "hello")
-       <p>Hi from inside the if statement</p>
-   @endif
+<x-layout>
+    <h2>Currently Available Ninjas</h2>
 
-   <ul>
-      @foreach ($ninjas as $ninja)
-          <li>
-            <p>{{ $ninja['name'] }}</p>
-            <a href="/ninjas/{{$ninja['id']}}">View Details</a>
-          </li>
-      @endforeach
-   </ul>
-</body>
-</html>
+    @if ($greeting == 'hello')
+        <p>Hi from inside the if statement
+        </p>
+    @endif
+
+    <ul>
+        @foreach ($ninjas as $ninja)
+            <li>
+                <x-card href="/ninjas/{{ $ninja['id'] }}" :highlight="$ninja['skill'] > 70">
+                    <h3>{{ $ninja['name'] }}</h3>
+                </x-card>
+            </li>
+        @endforeach
+    </ul>
+</x-layout>
